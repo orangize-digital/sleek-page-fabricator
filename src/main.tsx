9f70fe43540b3537307ef9a5eee03a6156093f45
orangize-digital <info@orangize.de>
2025-03-22 17:05:59 +0100

    adding /danke page

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Danke from "./components/Danke"; // Adjust the path if needed
-import NotFound from "./pages/NotFound"; // Optional
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/danke" element={<Danke />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
