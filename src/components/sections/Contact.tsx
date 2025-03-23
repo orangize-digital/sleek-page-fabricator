587bc37035f5da7f1e56c501a05bcbf711d955a9
Naumche Joshevski <naumejosevski@gmail.com>
2025-03-23 11:59:40 +0100

    working

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -183,7 +183,7 @@ const Contact = () => {
               method="POST"
               data-netlify="true"
               data-netlify-honeypot="bot-field"
-              action="/danke?no-cache=1"
+              action="/danke.html?no-cache=1"
               className="space-y-6"
             >
               {/* Netlify required hidden inputs */}
